512c8fc524080d6c845e2efdda985756466d32a5
mateenkhadem <86128603+mateenkhadem@users.noreply.github.com>
2025-02-09 13:47:34 +0330

    The Second commit

---
+++ hello-world/src/app/products/page.tsx
@@ -0,0 +1,12 @@
+export default function ProductsList() {
+    return (
+        <>
+            <h1>Products List</h1>
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </>
+    )
+}
